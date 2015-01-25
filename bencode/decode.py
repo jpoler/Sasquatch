e50b57bf68ce2bc08815f3f7c32ec1f0683fdb81
Jon Poler <jdp5087@gmail.com>
2015-01-25 08:48:25 -0500

    Decode is working!

--- bencode/decode.py
+++ bencode/decode.py
@@ -13,7 +13,7 @@ class Decoder(object):
 
     def parse_integer(self, offset, *args):
         e = self.bs.find(b'e', offset)
-        return (self.bs[offset:e], e+2)
+        return (int(self.bs[offset:e]), e+1)
 
     def parse_list(self, offset, *args):
         lst = []
@@ -28,7 +28,8 @@ class Decoder(object):
         dct = {}
         after_value = offset
         while self.bs[after_value] != ord(b'e'):
-            f, after_key_header, arg = self.type_header(offset)
+            print(self.bs[after_value])
+            f, after_key_header, arg = self.type_header(after_value)
             key, after_key = f(after_key_header, arg)
             g, after_value_header, arg2 = self.type_header(after_key)
             value, after_value = g(after_value_header, arg2)
@@ -38,12 +39,14 @@ class Decoder(object):
     
     def type_header(self, offset):
         print(chr(self.bs[offset]))
+        print(offset)
         if self.bs[offset] == ord(b'i'):
             return (self.parse_integer, offset+1, None)
         elif self.bs[offset] == ord(b'd'):
             return (self.parse_dict, offset+1, None)
         colon = self.bs.find(b':', offset)
         datatype = self.bs[offset:colon]
+        print(datatype)
         if datatype.isdigit():
             return (self.parse_string, colon + 1, int(datatype))
         elif datatype == b'l':
@@ -52,8 +55,8 @@ class Decoder(object):
 
     def decode(self):
         f, after_header, arg = self.type_header(0)
-        print(f, after_header, arg)
         obj, _ = f(after_header, arg)
+        return obj
   
 if __name__ == '__main__':
     TEST_FILE = os.path.join(conf.PROJECT_DIRECTORY, 'data', 'tom.torrent')
@@ -61,4 +64,5 @@ if __name__ == '__main__':
         text = f.read()
     print(type(text))
     d = Decoder(text)
+    print(text[112:])
     print(d.decode())
