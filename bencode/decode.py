0db6eccb5a7f70424c168b6f9580e172a426c5fe
Jon Poler <jdp5087@gmail.com>
2015-01-27 18:54:56 -0500

    Getting the info dict string out in a sort of hackish way, but I'm not too concerned.

--- bencode/decode.py
+++ bencode/decode.py
@@ -1,12 +1,19 @@
 import os
 
+import hashlib
+
 import bittorrent.conf as conf
+from collections import OrderedDict
 
 class Decoder(object):
 
-    def __init__(self, byte_string):
+    def __init__(self, byte_string, wanted_substring=None):
         self.bs = byte_string
         self.mv = memoryview(byte_string)
+        self.substrings = {}
+        # This is ugly but necessary, otherwise nasty surprised happen with
+        # always being given the same list reference, in the default keyword
+        self.wanted_substring = wanted_substring
 
     def parse_string(self, offset, length):
         return (self.bs[offset:offset+length], offset+length)
@@ -25,21 +32,26 @@ class Decoder(object):
         return (lst, next_offset+1)
 
     def parse_dict(self, offset, *args):
-        dct = {}
+        dct = OrderedDict()
         after_value = offset
         while self.bs[after_value] != ord(b'e'):
             print(self.bs[after_value])
             f, after_key_header, arg = self.type_header(after_value)
             key, after_key = f(after_key_header, arg)
+            
             g, after_value_header, arg2 = self.type_header(after_key)
             value, after_value = g(after_value_header, arg2)
+            print(key)
+            print(self.wanted_substring)
+            if key == self.wanted_substring:
+
+                print(type(key),type(after_key), type(after_value))
+                self.substrings[key] = self.bs[after_key:after_value]
             dct[key] = value
         return (dct, after_value+1)
         
     
     def type_header(self, offset):
-        print(chr(self.bs[offset]))
-        print(offset)
         if self.bs[offset] == ord(b'i'):
             return (self.parse_integer, offset+1, None)
         elif self.bs[offset] == ord(b'd'):
@@ -57,12 +69,23 @@ class Decoder(object):
         f, after_header, arg = self.type_header(0)
         obj, _ = f(after_header, arg)
         return obj
+
   
 if __name__ == '__main__':
     TEST_FILE = os.path.join(conf.PROJECT_DIRECTORY, 'data', 'tom.torrent')
     with open(TEST_FILE, 'rb') as f:
         text = f.read()
     print(type(text))
-    d = Decoder(text)
-    print(text[112:])
+    d = Decoder(text, b'info')
     print(d.decode())
+    print(d.substrings)
+
+    shablam = hashlib.sha1()
+    shablam2 = shablam.copy()
+    
+    shablam.update(d.substrings[b'info'])
+    print(shablam.digest())
+    shablam2.update(d.substrings[b'info'][1:-1])
+    print(shablam2.digest())
+    
+
